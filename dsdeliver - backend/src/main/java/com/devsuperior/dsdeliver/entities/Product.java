7453a140ddd2006fd3181fe2444a6fc3d84e037f
Victor Lucas <victorlucas.ao@gmail.com>
2021-01-05 16:07:24 -0400

    Domain model
    Problema resolvido.
    
    Obs: Falta de atenção, @Table(name) name está errado...

--- dsdeliver - backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
+++ dsdeliver - backend/src/main/java/com/devsuperior/dsdeliver/entities/Product.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
 
 @Data
 @Entity
-@Table(name = "tb_products")
+@Table(name = "tb_product")
 public class Product implements Serializable {
     // O Serializable é uma boa prática, para que nosso objeto Products possam ser transformados em sequências de Bytes, para evitar problemas na rede na hora de acessar arquivos e etc.
     private static  final long serialVersionUID = 1L;
