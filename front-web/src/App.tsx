62d2d98e17b21f4407206d5a90211d01259fb6eb
Victor Lucas <victorlucas.ao@gmail.com>
2021-01-08 18:25:46 -0400

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,20 @@
-import './App.css';
-import NavbarApp from './templates/navbar/app-navbar';
+import "./App.css";
+import NavbarApp from "./templates/navbar/app-navbar";
+import HomeApp from "./templates/home/app-home";
 
 function App() {
   return (
-    <div>
-      <NavbarApp/>
-    </div>
+    <body>
+      <header>
+        <NavbarApp />
+      </header>
+
+      <main>
+        <HomeApp />
+      </main>
+
+      <footer></footer>
+    </body>
   );
 }
 
---
+++ front-web/src/templates/home/app-home.tsx
@@ -0,0 +1,29 @@
+import '../home/app-home.css'
+import {ReactComponent as MainImg} from '../home/main-img.svg'
+function HomeApp(){
+  return(
+    <div className="home-container">
+      <div className="home-content">
+        <div className="home-actions">
+          <h1 className="home-title">
+            Faça seu pedido <br/>
+            que entregamos<br/>
+            pra você!!!
+          </h1>
+          <h3 className="home-subtitle">
+            Escolha o seu pedido e em poucos minutos<br/>
+            levaremoss na sua porta
+          </h3>
+          <a href="#" className="home-btn-order">
+            Fazer pedido
+          </a>
+        </div>
+        <div className="home-img">
+          <MainImg/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeApp;
